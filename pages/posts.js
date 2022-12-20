9af0855aac6b55d36e909ace29c7bce8763a7970
Annie <anniehartmann97@gmail.com>
2022-12-20 15:40:21 +0300

    Api

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
 import { MainLayout } from "../components/MainLayout";
+import styles from "../styles/Posts.module.scss";
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    load();
+  }, []);
+
   return (
     <MainLayout titleText="Posts">
-      <h1>Posts</h1>
+      <h1 className={styles.postsTitle}>Posts</h1>
+      <div className={styles.posts}>
+        {posts.map(({ id, title, body }) => (
+          <div className={styles.post} key={id}>
+            <p>
+              {id}. {title}
+            </p>
+            <p>{body}</p>
+          </div>
+        ))}
+      </div>
     </MainLayout>
   );
 }
