ce0bc4203a03f82f779fd5bac164a913ec209a16
Annie <anniehartmann97@gmail.com>
2022-12-20 18:10:30 +0300

    Add loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        color="#f8f9fa"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,16 +1,25 @@
 import Router from "next/router";
 import { MainLayout } from "../../components/MainLayout";
 
-export default function About() {
+export default function About({ title }) {
   const linkClickHandler = () => {
     Router.push("/");
   };
 
   return (
     <MainLayout titleText="About">
-      <h1>About</h1>
+      <h1>{title}</h1>
 
       <button onClick={linkClickHandler}>Go back to home</button>
     </MainLayout>
   );
 }
+
+About.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/about");
+  const data = await response.json();
+
+  return {
+    title: data.title,
+  };
+};
