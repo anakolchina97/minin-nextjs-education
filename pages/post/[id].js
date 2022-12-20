e2225cc5123ea286389ce2f64de22d336e457472
Annie <anniehartmann97@gmail.com>
2022-12-20 16:27:52 +0300

    Get one post

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,23 @@
 import { useRouter } from "next/router";
+import { MainLayout } from "../../components/MainLayout";
 
-export default function Post() {
+export default function Post({ post }) {
   const router = useRouter();
+  const { title, body } = post;
   return (
-    <>
-      <h1>Post {router.query.id}</h1>
-    </>
+    <MainLayout titleText={"Post"}>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </MainLayout>
   );
 }
+
+Post.getInitialProps = async (ctx) => {
+  console.log(ctx);
+  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+  const post = await response.json();
+
+  return {
+    post,
+  };
+};
--- pages/posts.js
+++ pages/posts.js
@@ -1,33 +1,29 @@
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { MainLayout } from "../components/MainLayout";
 import styles from "../styles/Posts.module.scss";
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function load() {
-      const response = await fetch("http://localhost:4200/posts");
-      const json = await response.json();
-      setPosts(json);
-    }
-
-    load();
-  }, []);
-
+export default function Posts({ posts }) {
   return (
     <MainLayout titleText="Posts">
       <h1 className={styles.postsTitle}>Posts</h1>
       <div className={styles.posts}>
         {posts.map(({ id, title, body }) => (
-          <div className={styles.post} key={id}>
-            <p>
-              {id}. {title}
-            </p>
-            <p>{body}</p>
-          </div>
+          <Link href={`/post/${id}`} className={styles.post} key={id}>
+            {title}
+          </Link>
         ))}
       </div>
     </MainLayout>
   );
 }
+
+//  статический метод, который будет выполняться на сервере
+Posts.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/posts");
+  const posts = await response.json();
+
+  return {
+    posts,
+  };
+};
